4fc06d0ff02dfbd7d16c596e8634a3beaeac99a0
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-05-13 21:37:10 +0200

    -m

--- ES6_basic/12-createReportObject.js
+++ ES6_basic/12-createReportObject.js
@@ -1,9 +1,10 @@
 export default function createReportObject(employeesList) {
-    return {
-      allEmployees: { ...employeesList },
-      getNumberOfDepartments(allEmployees) {
-        return Object.keys(allEmployees).length;
-      },
-    };
-  }
-  
+  return {
+    allEmployees: {
+      ...employeesList,
+    },
+    getNumberOfDepartments(allEmployees) {
+      return Object.keys(employeesList).length;
+    },
+  };
+}
