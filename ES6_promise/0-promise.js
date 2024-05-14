e3d7f1677342d8c02fbe2497a44b0cafeb6f3d7b
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-05-14 19:14:59 +0200

    -m

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,8 +1,10 @@
-function getResponseFromAPI() {
-    return new Promise((resolve) => {
-      resolve('Success');
+export default function getResponseFromAPI() {
+    return new Promise((resolve, reject) => {
+      const fakeResponse = {
+        status: 200,
+        body: 'Fake response from API',
+      };
+      resolve(fakeResponse);
     });
   }
-  
-  module.exports = getResponseFromAPI;
   
