c202b40401dddbc16b1b2f51e4e118eb08d2af3e
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-05-16 12:18:22 +0200

    -m

--- ES6_promise/3-all.js
+++ ES6_promise/3-all.js
@@ -3,9 +3,8 @@ import { uploadPhoto, createUser } from './utils';
 export default function handleProfileSignup() {
   return Promise.all([uploadPhoto(), createUser()])
     .then((responses) => {
-      console.log(`${responses[0].body} ${responses[1].firstName} ${responses[1].lastName}`);
+      const [photo, user] = responses;
+      console.log(`${photo.body} ${user.firstName} ${user.lastName}`);
     })
-    .catch(() => {
-      console.log('Signup system offline');
-    });
+    .catch(() => console.log('Signup system offline'));
 }
