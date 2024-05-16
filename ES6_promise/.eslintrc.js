b311ce853a38bcbb1ba3461acbcb43d8939124a1
Miniknacky <Jonathanhuybrechts@gmail.com>
2024-05-16 12:07:30 +0200

    -m

--- ES6_promise/.eslintrc.js
+++ ES6_promise/.eslintrc.js
@@ -1,22 +1,36 @@
 module.exports = {
-    env: {
-      browser: true,
-      es2021: true,
-      node: true,
-      jest: true,
-    },
-    extends: ['eslint:recommended', 'plugin:react/recommended', 'airbnb', 'prettier'],
-    parserOptions: {
-      ecmaFeatures: {
-        jsx: true,
-      },
-      ecmaVersion: 12,
-      sourceType: 'module',
-    },
-    plugins: ['react', 'prettier'],
-    rules: {
-      'prettier/prettier': 'error',
-      'react/jsx-filename-extension': [1, { extensions: ['.js', '.jsx'] }],
-    },
-  };
-  
+  env: {
+    browser: false,
+    es6: true,
+    jest: true,
+  },
+  extends: [
+    'airbnb-base',
+    'plugin:jest/all',
+  ],
+  globals: {
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
+  },
+  parserOptions: {
+    ecmaVersion: 2018,
+    sourceType: 'module',
+  },
+  plugins: ['jest'],
+  rules: {
+    'no-console': 'off',
+    'no-shadow': 'off',
+    'no-restricted-syntax': [
+      'error',
+      'LabeledStatement',
+      'WithStatement',
+    ],
+  },
+  overrides:[
+    {
+      files: ['*.js'],
+      excludedFiles: 'babel.config.js',
+    }
+  ]
+};
+
