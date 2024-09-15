55bc10864de03ac62baecc3563fe897a439b64a0
JHMindSharp <Jonathanhuybrechts@gmail.com>
2024-09-16 01:46:16 +0200

    maj task 5 calling task 3

--- Node_JS_basic/5-http.js
+++ Node_JS_basic/5-http.js
@@ -1,32 +1,5 @@
 const http = require('http');
-const fs = require('fs');
-
-function countStudents(path) {
-  return new Promise((resolve, reject) => {
-    fs.readFile(path, 'utf8', (err, data) => {
-      if (err) {
-        reject(new Error('Cannot load the database'));
-        return;
-      }
-
-      const lines = data.trim().split('\n').slice(1);
-      const students = lines.filter(line => line).map(line => line.split(','));
-      const numberOfStudents = students.length;
-
-      const fields = {};
-      students.forEach((student) => {
-        const field = student[3];
-        const firstName = student[0];
-        if (!fields[field]) {
-          fields[field] = [];
-        }
-        fields[field].push(firstName);
-      });
-
-      resolve({ numberOfStudents, fields });
-    });
-  });
-}
+const countStudents = require('./3-read_file_async');
 
 const app = http.createServer((req, res) => {
   if (req.url === '/') {
@@ -34,15 +7,16 @@ const app = http.createServer((req, res) => {
     res.end('Hello Holberton School!');
   } else if (req.url === '/students') {
     countStudents(process.argv[2])
-      .then(({ numberOfStudents, fields }) => {
+      .then((fields) => {
         res.writeHead(200, { 'Content-Type': 'text/plain' });
         res.write('This is the list of our students\n');
-        res.write(`Number of students: ${numberOfStudents}\n`);
-        
+        const totalStudents = Object.values(fields).reduce((acc, curr) => acc + curr.length, 0);
+        res.write(`Number of students: ${totalStudents}\n`);
         for (const field in fields) {
-          res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+          if (fields.hasOwnProperty(field)) {
+            res.write(`Number of students in ${field}: ${fields[field].length}. List: ${fields[field].join(', ')}\n`);
+          }
         }
-
         res.end();
       })
       .catch((error) => {
